6fb41dc3a8e37e521b44f90c90ffd27fe3b653c8
Ziad Malik <z.malik@surreyschools.ca>
2024-05-05 11:29:56 -0700

    added support to clean up sata read from file

--- src/java/BinaryToDecimal.java
+++ src/java/BinaryToDecimal.java
@@ -6,13 +6,15 @@ public class BinaryToDecimal {
 	public static void main(String[] args) throws FileNotFoundException{
 		String [] binaryNums = new String[100];
 		Scanner sc = new Scanner(new File("src/binarydata.txt"));
+		int index = 0;
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 			//System.out.println(line);
 			String[] parts = line.split(",");
 			for (int i = 0; i < parts.length; i++) {
-				System.out.println(parts[i]);
-				binaryNums[i] = parts[i];
+				//System.out.println(parts[i].trim());
+				binaryNums[index] = parts[i].trim();
+				index++;
 			}
 		}
 		for (int i = 0; i < binaryNums.length; i++) {
