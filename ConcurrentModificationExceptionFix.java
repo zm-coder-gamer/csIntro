3368dd6cfe41bd610267b654cffb3b4b77674c2e
Ziad Malik <61949743+zm-coder-gamer@users.noreply.github.com>
2024-06-22 23:49:02 -0700

    Create ConcurrentModificationExceptionFix.java

---
+++ ConcurrentModificationExceptionFix.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyClass {
+    public static void main(String args[]) {
+      // Your array
+      List<String> myArray = new ArrayList<>();
+      // things in the array
+      myArray.add("Ziad");
+      myArray.add("Ziad1");
+      myArray.add("Ziad2");
+      myArray.add("Ziad3");
+      System.out.println(myArray);
+      //condition to set in the for loop
+      boolean removename = false;
+      //loop
+        for (String xyz: myArray) {
+          //  if statement
+          if (xyz == "Ziad2") {    
+            // set condition to true    
+            removename=true;
+            }
+        }
+      // remove the item outside your loop
+        if (removename){
+            myArray.remove("Ziad2");
+        }
+      System.out.println(myArray);
+    }
+}
