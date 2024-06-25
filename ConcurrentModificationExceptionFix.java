109898fd6afd0a0c42d6539bae43220c23927372
Ziad Malik <z.malik@surreyschools.ca>
2024-06-24 19:24:50 -0700

    implements the correct way to deal with a situation thats causes a concurrentmodificationexception

--- ConcurrentModificationExceptionFix.java
+++ ConcurrentModificationExceptionFix.java
@@ -13,16 +13,19 @@ public class ConcurrentModificationExceptionFix {
     List<String> names = new ArrayList<>();
     // things in the array
     names.add("Ziad");
-    names.add("Ziad1");
-    names.add("Ziad2");
-    names.add("Ziad3");
+    names.add("Johnathan");
+    names.add("Lily");
+    names.add("Nick");
+    names.add("Richard");
+    names.add("Dorothy");
+    names.add("Jim");
     System.out.println(names);
     //condition to set in the for loop
     //loop
     String nametodelete = "";
     try {
       for (String name : names) {
-        if (name.contains("1")) {
+        if (name.length() < 5) {
           nametodelete = name;
           names.remove(name);
         }
@@ -32,6 +35,17 @@ public class ConcurrentModificationExceptionFix {
       System.out.println("Caught ConcurrentModificationException while deleting [" + nametodelete + "]");
     }
 
+    System.out.println(names);
+    // lets do it the right way
+    List<String> namestodelete = new ArrayList<>();
+    for (String name : names) {
+      if (name.length() < 5) {
+        namestodelete.add(name);
+      }
+    }
+    if (namestodelete.size() > 0) {
+      names.removeAll(namestodelete);
+    }
     System.out.println(names);
   }
 }
